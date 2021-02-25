1f22cc3b5a82fe675c0c79d22472231a336e2035
Anton Boksha <antonboksha@gmail.com>
2021-02-25 11:32:36 +0200

    A long time ago in a galaxy far, far away....

---
+++ src/background.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+
+try {
+  require('electron-reloader')(module);
+} catch { }
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: true,
+    }
+  })
+
+  // TODO: use builded index.html instead of running vue dev server
+  // win.loadFile('dist/index.html')
+  win.loadURL('http://localhost:3000')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  // if (process.platform !== 'darwin') {
+  app.quit()
+  // }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
