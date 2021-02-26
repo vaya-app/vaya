fa8956846ecead5944fd84be0fd009712c8c7163
4doge <antonboksha@gmail.com>
2021-02-26 20:25:22 +0200

    add eslint

--- src/components/Placeholder.vue
+++ src/components/Placeholder.vue
@@ -2,9 +2,16 @@
   <div class="hero is-fullheight">
     <div class="hero-body has-text-centered">
       <div class="container">
-        <h1 class="title">Vaya 🔥</h1>
-        <p class="subtitle">Something <strong>special</strong> coming 👀</p>
-        <button class="button is-danger" @click="count++">
+        <h1 class="title">
+          Vaya 🔥
+        </h1>
+        <p class="subtitle">
+          Something <strong>special</strong> coming 👀
+        </p>
+        <button
+          class="button is-danger"
+          @click="count++"
+        >
           ❤️ &lt;=&gt; {{ count }}
         </button>
       </div>
