4e25b74ac8dddea51b2c95fa2730e7995adf783f
YuraNosenko <nosenko.yur@gmail.com>
2021-04-03 23:31:40 +0300

    Command line was created

---
+++ src/components/tabs/CommandLine.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>
+   {{ command.content }}
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'CommandLine',
+  props: {
+    command: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- src/components/tabs/Shell.vue
+++ src/components/tabs/Shell.vue
@@ -1,9 +1,59 @@
 <template>
-  <p class="subtitle">Shell content will be here. Stay tuned. 👨‍💻</p>
+  <div>
+    <div class="content">
+      <!-- <CommandLine
+        v-for="command in commands"
+        :key="command"
+        :command="command"
+      /> -->
+      <CommandLine/>
+    </div>
+    <input
+      v-model="command"
+      @keyup.enter="runCommand"
+      class="input is-medium"
+      type="text"
+      placeholder="Please enter your request"
+    />
+  </div>
 </template>
 
 <script>
+import CommandLine from '@/components/tabs/CommandLine.vue';
+
 export default {
   name: 'Shell',
+  components: {
+    CommandLine,
+  },
+  data() {
+    return {
+      command: '',
+      commands: [],
+    };
+  },
+  mounted() {
+    console.log(CommandLine);
+  },
+  methods: {
+    runCommand() {
+      const newLine = {
+        isCmd: true,
+        content: this.command,
+      };
+      this.commands.push(newLine);
+      this.command = '';
+    },
+  },
 };
 </script>
+
+<style scoped>
+  .content {
+    border: 2px solid teal;
+    padding: 10px 20px;
+    border-radius: 10px;
+    height: 300px;
+    overflow: scroll;
+  }
+</style>
